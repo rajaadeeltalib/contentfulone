77ec1c316e113202fecd18dfa9ceca79635b1a57
rajaadeeltalib <adeeltalib6270@gmail.com>
2023-04-18 09:20:03 +0500

    project created

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
+  },
+  env: {
+    CONTENTFUL_SPACE_ID: "1g59yh9r4kc9",
+    CONTENTFUL_ACCESS_KEY: "vKSKh8sCqhd9M--o7kvfa1E76Q5-T8ZCzUqL6u8SoO4"
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.ctfassets.net',
+        port: '',
+      },
+    ],
   },
 }
 
